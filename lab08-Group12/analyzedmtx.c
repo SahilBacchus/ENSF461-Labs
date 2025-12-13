734a36ff2d87ecef4584c258bf98ec17a8b6ea3e
Sahil <sahilbacchus@gmail.com>
2025-12-13 23:07:11 +0000

    upload lab08 we handed in

--- lab08-Group12/analyzedmtx.c
+++ lab08-Group12/analyzedmtx.c
@@ -9,117 +9,60 @@
 #include <dmtx.h>
 #include <wand/magick-wand.h>
 #include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TRUE 1
 #define FALSE 0
-
-// Used to store the file names and result messages
 #define MAX_FNAME_LENGTH 256
 #define MAX_MESSAGE_LENGTH 8000
+#define SEM_NAME "/dmtx_sem"
 
-// Data structure to store filename/message
-// You may wish to modify this for your solution
 typedef struct {
     char filename[MAX_FNAME_LENGTH];
     char message[MAX_MESSAGE_LENGTH];
 } filedata;
 
-// FD for shared memory object
+typedef struct {
+    int next_index;
+} shared_info;
+
+shared_info* info = NULL;
+filedata* filelist = NULL;
 int sharedfd;
-
-// Ptr to shared memory object
-// Note, this needs to be reinitialized after a fork
-char* sharedmem;
-
-// Number of files in the directory
+char* sharedmem = NULL;
 int numfiles = 0;
+char folderpath[MAX_FNAME_LENGTH];
 
-// Generate a list of files in the directory
-// Must create a shared memory object in the "sharedfd" global variable
-// Must create a shared memory mapping in the "sharedmem" global variable
-// Must set the "numfiles" global variable
-// Must return TRUE on success, FALSE on failure
-int generate_file_list(char* path) {
-    DIR *dir;
-    struct dirent *ent;
-    numfiles = 0;
-    if ((dir = opendir (path)) != NULL) {
-        while ((ent = readdir (dir)) != NULL) {
-            if (strstr(ent->d_name, ".png") != NULL) {
-                numfiles++;
-            }
-        }
-        closedir (dir);
-    } else {
-        perror ("");
-        return FALSE;
-    }
-    
-    // Then, create a shared memory objects, resize it to store the file list,
-    // mmap it, and fill in the file list
-    sharedfd = shm_open("filelist", O_CREAT | O_RDWR, 0666);
-    if (sharedfd < 0) {
-        perror("shm_open");
-        return -1;
-    }
-    if (ftruncate(sharedfd, sizeof(filedata) * numfiles) < 0) {
-        perror("ftruncate");
-        return FALSE;
-    }
-    sharedmem = mmap(NULL, sizeof(filedata) * numfiles, PROT_READ | PROT_WRITE, MAP_SHARED, sharedfd, 0);
-    if (sharedmem == MAP_FAILED) {
-        perror("mmap");
-        return FALSE;
-    }
-
-    //Copy over the files into the sharedmem array
-
-
-    return TRUE;
-}
-
-// Keep track whether MagickWand was initialized
 int initialized = 0;
 
-// Initialize MagickWand
 void initdmtx() {
-    if (initialized) {
-        return;
-    }
-    MagickWandGenesis();
-    initialized = TRUE;
-}
-
-// Close MagickWand
-void closedmtx() {
     if (!initialized) {
-        return;
+        MagickWandGenesis();
+        initialized = 1;
     }
-    MagickWandTerminus();
-    initialized = FALSE;
 }
 
-//Process an individual dtmx file and return the encoded message as a string
+void closedmtx() {
+    if (initialized) {
+        MagickWandTerminus();
+        initialized = 0;
+    }
+}
+
 char* scandmtx(char* filepath) {
-    //These variables have the same name as the source dmtxread.c file
-    //Hint: You'll need to implement the code to use all of these variables  
     int width, height;
-    unsigned char* pxl;
-    MagickBooleanType success;
-    DmtxImage* img;
-    DmtxDecode* dec;
-    DmtxRegion* reg;
-    DmtxMessage* msg;
+    unsigned char* pxl = NULL;
+    DmtxImage* img = NULL;
+    DmtxDecode* dec = NULL;
+    DmtxRegion* reg = NULL;
+    DmtxMessage* msg = NULL;
     char* result = NULL;
     MagickWand* wand;
 
-    if (!initialized) {
-        initdmtx();
-        initialized = 1;
-    }
+    if (!initialized) initdmtx();
 
     wand = NewMagickWand();
-
     if (MagickReadImage(wand, filepath) == MagickFalse) {
         DestroyMagickWand(wand);
         return NULL;
@@ -127,51 +70,26 @@ char* scandmtx(char* filepath) {
 
     width = MagickGetImageWidth(wand);
     height = MagickGetImageHeight(wand);
-    pxl = (unsigned char *)malloc(3 * width * height * sizeof(unsigned char));
-    
-    success = MagickExportImagePixels(wand, 0, 0, width, height, "RGB", CharPixel, pxl);
-    if ( success == MagickFalse ) {
-        DestroyMagickWand(wand);
-        return NULL;
+
+    pxl = malloc(3 * width * height);
+    if (!pxl) { DestroyMagickWand(wand); return NULL; }
+
+    if (MagickExportImagePixels(wand, 0, 0, width, height, "RGB", CharPixel, pxl) == MagickFalse) {
+        free(pxl); DestroyMagickWand(wand); return NULL;
     }
 
     img = dmtxImageCreate(pxl, width, height, DmtxPack24bppRGB);
-    if (img == NULL) {
-        DestroyMagickWand(wand);
-        return NULL;
-    }
-
-    //Look at dmtxread.c to implement the rest of the dmtx decoding process
-    
+    if (!img) { free(pxl); DestroyMagickWand(wand); return NULL; }
 
     dec = dmtxDecodeCreate(img, 1);
-    if (!dec) {
-        dmtxImageDestroy(&img);
-        DestroyMagickWand(wand);
-        free(pxl);
-        return NULL;
-    }
+    if (!dec) { dmtxImageDestroy(&img); free(pxl); DestroyMagickWand(wand); return NULL; }
 
     reg = dmtxRegionFindNext(dec, NULL);
-    if (!reg) {
-        dmtxDecodeDestroy(&dec);
-        dmtxImageDestroy(&img);
-        DestroyMagickWand(wand);
-        free(pxl);
-        return NULL;
-    }
+    if (!reg) { dmtxDecodeDestroy(&dec); dmtxImageDestroy(&img); free(pxl); DestroyMagickWand(wand); return NULL; }
 
     msg = dmtxDecodeMatrixRegion(dec, reg, 0);
-    if (!msg) {
-        dmtxRegionDestroy(&reg);
-        dmtxDecodeDestroy(&dec);
-        dmtxImageDestroy(&img);
-        DestroyMagickWand(wand);
-        free(pxl);
-        return NULL;
-    }
+    if (!msg) { dmtxRegionDestroy(&reg); dmtxDecodeDestroy(&dec); dmtxImageDestroy(&img); free(pxl); DestroyMagickWand(wand); return NULL; }
 
-    // Copy the message
     result = malloc(msg->outputIdx + 1);
     memcpy(result, msg->output, msg->outputIdx);
     result[msg->outputIdx] = '\0';
@@ -179,68 +97,116 @@ char* scandmtx(char* filepath) {
     dmtxMessageDestroy(&msg);
     dmtxRegionDestroy(&reg);
     dmtxDecodeDestroy(&dec);
-
-
-    
-
     dmtxImageDestroy(&img);
+    free(pxl);
     DestroyMagickWand(wand);
 
     return result;
 }
 
-//Code for your Sequential Implementation 
+int generate_file_list(char* path, int include_info) {
+    DIR* dir;
+    struct dirent* ent;
+    numfiles = 0;
+
+    if ((dir = opendir(path)) == NULL) { perror("opendir"); return FALSE; }
+    while ((ent = readdir(dir)) != NULL) {
+        if (strstr(ent->d_name, ".png")) numfiles++;
+    }
+    closedir(dir);
+
+    int total_size = sizeof(filedata) * numfiles + (include_info ? sizeof(shared_info) : 0);
+
+    shm_unlink("filelist");
+    sharedfd = shm_open("filelist", O_CREAT | O_RDWR, 0666);
+    if (sharedfd < 0) { perror("shm_open"); return FALSE; }
+    if (ftruncate(sharedfd, total_size) < 0) { perror("ftruncate"); return FALSE; }
+
+    sharedmem = mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_SHARED, sharedfd, 0);
+    if (sharedmem == MAP_FAILED) { perror("mmap"); return FALSE; }
+
+    filelist = include_info ? (filedata*)(sharedmem + sizeof(shared_info)) : (filedata*)sharedmem;
+
+    if ((dir = opendir(path)) == NULL) { perror("opendir"); return FALSE; }
+    int idx = 0;
+    while ((ent = readdir(dir)) != NULL) {
+        if (strstr(ent->d_name, ".png")) {
+            strncpy(filelist[idx].filename, ent->d_name, MAX_FNAME_LENGTH);
+            filelist[idx].message[0] = '\0';
+            idx++;
+        }
+    }
+    closedir(dir);
+    return TRUE;
+}
+
 void generate_dmtx_seq() {
-    filedata* filelist = (filedata*)sharedmem;
-
-    for ( int i = 0; i < numfiles; i++) {
-        // Get and save the message from each of the files in the dtmx folder 
-    } 
-
+    for (int i = 0; i < numfiles; i++) {
+        char fullpath[512];
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", folderpath, filelist[i].filename);
+        char* msg = scandmtx(fullpath);
+        if (msg) { strncpy(filelist[i].message, msg, MAX_MESSAGE_LENGTH); free(msg); }
+        else snprintf(filelist[i].message, MAX_MESSAGE_LENGTH, "[ERROR decoding %s]\n", filelist[i].filename);
+    }
     closedmtx();
 }
 
-//Code for your Parallel Implementation
-void generate_dmtx_par() {
-    filedata* filelist = (filedata*)sharedmem;
+void generate_dmtx_par(int numprocesses) {
+    info = (shared_info*)sharedmem;
+    info->next_index = 0;
 
-    //Implement your parallel code solution here!
+    sem_t* sem = sem_open(SEM_NAME, O_CREAT, 0666, 1);
+    if (sem == SEM_FAILED) { perror("sem_open"); exit(1); }
 
+    for (int p = 0; p < numprocesses; p++) {
+        pid_t pid = fork();
+        if (pid < 0) { perror("fork"); exit(1); }
+        if (pid == 0) {
+            sem_t* child_sem = sem_open(SEM_NAME, 0);
+            if (child_sem == SEM_FAILED) { perror("sem_open child"); exit(1); }
+            while (1) {
+                sem_wait(child_sem);
+                int idx = info->next_index++;
+                sem_post(child_sem);
+                if (idx >= numfiles) break;
+
+                char fullpath[512];
+                snprintf(fullpath, sizeof(fullpath), "%s/%s", folderpath, filelist[idx].filename);
+                char* msg = scandmtx(fullpath);
+                if (msg) { strncpy(filelist[idx].message, msg, MAX_MESSAGE_LENGTH); free(msg); }
+                else snprintf(filelist[idx].message, MAX_MESSAGE_LENGTH, "[ERROR decoding %s]\n", filelist[idx].filename);
+            }
+            sem_close(child_sem);
+            closedmtx();
+            exit(0);
+        }
+    }
+
+    for (int p = 0; p < numprocesses; p++) wait(NULL);
+    sem_close(sem);
+    sem_unlink(SEM_NAME);
     closedmtx();
 }
 
 int main(int argc, char** argv) {
-    if ( argc != 4 ) {
-        printf("Usage: %s <#processes> <folder> <output file>\n", argv[0]);
-        return 1;
-    }
-
+    if (argc != 4) { printf("Usage: %s <#processes> <folder> <output>\n", argv[0]); return 1; }
     int numprocesses = atoi(argv[1]);
+    strncpy(folderpath, argv[2], MAX_FNAME_LENGTH);
 
-    // Build list of DMTX files in the directory
-    // Note, any PNG file will be considered a DMTX code
- 
-    int result = generate_file_list(argv[2]);
-    if ( result == FALSE )
-        return -1;
-    
-    if ( numprocesses == 0 )
-        generate_dmtx_seq();
+    if (numprocesses == 0) generate_file_list(folderpath, 0);
+    else generate_file_list(folderpath, 1);
 
-    // Write the results to the output file
+    if (numprocesses == 0) generate_dmtx_seq();
+    else generate_dmtx_par(numprocesses);
+
     FILE* fp = fopen(argv[3], "w");
-    if (fp == NULL) {
-        perror("fopen");
-        return -1;
-    }
-    filedata* filelist = (filedata*)sharedmem;
-    for ( int i = 0; i < numfiles; i++ ) {
-        fprintf(fp, "%s", filelist[i].message);
-    }
+    if (!fp) { perror("fopen"); return -1; }
+    for (int i = 0; i < numfiles; i++) fprintf(fp, "%s", filelist[i].message);
     fclose(fp);
 
-    munmap(sharedmem, sizeof(filedata) * numfiles);
+    int total_size = sizeof(filedata) * numfiles + (numprocesses > 0 ? sizeof(shared_info) : 0);
+    munmap(sharedmem, total_size);
     shm_unlink("filelist");
-    
+
     return 0;
 }
